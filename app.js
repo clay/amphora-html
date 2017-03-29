ca669b5e352d6314aa6c133d0eb860509cf689fa
Jon Winton <winton.jon@gmail.com>
2017-03-29 11:43:22 -0400

    Initial commit of barely functioning files; sstill unstable

--- app.js
+++ app.js
@@ -1,13 +1,65 @@
-'use strict'
-const express = require('express'),
-  app = express(),
-  port = process.env.PORT || 9000,
-  bodyParser = require('body-parser');
+'use strict';
 
-// nginx limit is also 1mb, so can't go higher without upping nginx
-app.use(bodyParser.json({limit: '1mb'}));
-app.use(bodyParser.urlencoded({limit: '1mb', extended: true}));
+var multiplex;
+const _ = require('lodash'),
+  multiplexTemplates = require('multiplex-templates'),
+  media = require('./lib/media'),
+  files = require('./lib/files'),
+  glob = require('glob'),
+  path = require('path'),
+  defaultHtmlTemplateName = 'template',
+  config = {
+    responseType: 'html'
+  };
+
+/**
+ * Check root of their project, or the 1st level of their node_modules
+ *
+ * @param {string} name
+ * @param {string} templateName  'template', 'apple', 'facebook'
+ * @returns {string[]}  List of all templates available.
+ */
+function getPossibleTemplates(name, templateName) {
+  const filePath = files.getComponentPath(name);
 
 
-app.get('/', (req, res) => res.send('Hello'));
-app.listen(port);
+  if (_.isString(filePath)) {
+    return glob.sync(path.join(filePath, templateName + '.*'));
+  }
+
+  return [];
+}
+
+/**
+ * [renderComponent description]
+ * @param  {[type]} data [description]
+ * @return [type]        [description]
+ */
+function render(data) {
+  const template = _.get(getPossibleTemplates(data.template, defaultHtmlTemplateName), '[0]', undefined);
+
+  if (!template) {
+    throw new Error('Missing template for ' + data.template);
+  }
+
+  return Promise.resolve(multiplex.render(template, data))
+    .then(media.append(data))
+    .then(result => {
+      return {
+        output: result,
+        type: config.responseType
+      }
+    });
+}
+
+/**
+ * [addEngines description]
+ * @param {[type]} engines [description]
+ */
+function addEngines(engines) {
+  multiplex = multiplexTemplates(engines);
+}
+
+module.exports.addEngines = addEngines;
+module.exports.render = render;
+module.exports.config = config;
