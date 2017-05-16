12ed91fefb3b16b9ab419da3877072488393f230
Jon Winton <winton.jon@gmail.com>
2017-05-16 13:43:06 -0400

    Comments and some cleanup

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 const setup = require('./lib/setup');
 
 module.exports.render = require('./lib/render');
-module.exports.addEngines = setup.addEngines;
 module.exports.addRootPath = setup.addRootPath;
+module.exports.addHelpers = setup.addHelpers;
