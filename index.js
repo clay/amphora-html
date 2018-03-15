a0d50c6f14373f914171761f4a14d3708c842c33
Jon Winton <winton.jon@gmail.com>
2018-03-15 14:11:09 -0400

    rough env vars

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const setup = require('./lib/setup'),
   render = require('./lib/render');
 
 module.exports.render = render;
+module.exports.setEnvVars = render.setEnvVars;
 module.exports.addResolveMedia = setup.addResolveMedia;
 module.exports.addRootPath = setup.addRootPath;
 module.exports.addHelpers = setup.addHelpers;
