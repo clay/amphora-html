7b398e2046fbc43d534cf7428042e03065861245
Jon Winton <winton.jon@gmail.com>
2018-03-26 16:16:25 -0400

    fixing package.json and renaming

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const setup = require('./lib/setup'),
   render = require('./lib/render');
 
 module.exports.render = render;
-module.exports.setEnvVars = render.setEnvVars;
+module.exports.addEnvVars = render.addEnvVars;
 module.exports.addResolveMedia = setup.addResolveMedia;
 module.exports.addHelpers = setup.addHelpers;
 module.exports.addPlugins = setup.addPlugins;
